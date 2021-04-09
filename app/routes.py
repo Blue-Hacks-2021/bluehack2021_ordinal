bb1fac3ed497d522508dc664444ad1d16fadc5aa
zachd123 <ao.tarriela7@icloud.com>
2021-04-09 12:47:35 +0800

    working on log in

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,9 @@ from app.forms import LoginForm, RegistrationForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'ProLoad'}
+    cur = mysql.connection.cursor()
+    
+    user = {'username': username}
     return render_template('index.html', title='Home', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -17,26 +19,26 @@ def login():
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def registration():
     form = RegistrationForm()
     if request.method == "POST":
-        details = request.form
         #as always, switch to token if needed
-        firstName = details['fname']
-        lastName = details['lname']
-        username = details['uname']
-        middlename = details['mname']
-        password = details['pass']
-        city = details['city']
-        province = details['province']
-        email = details['email']
+        firstName = form.firstName.data
+        lastName = form.lastName.data
+        username = form.userame.data
+        middlename = form.middleName.data
+        password = form.password.data
+        city = form.city.data
+        province = form.province.data
+        email = form.email.data
         cur = mysql.connection.cursor()
         if firstName != '' and lastName != '' and username != '' and password != '' and city != '' and province != '' and email != '':
-
             #maybe we can use stored procedures instead of hardcoding each and every query. Let's explore that option once we've built the basics
             cur.execute("INSERT INTO userdatatb(firstname, lastname, username, middlename, pass, city, province, email) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)", (firstName, lastName,username, middlename, password, city, province, email))
             mysql.connection.commit()
         cur.close()
-    return redirect('/login')
+        return redirect('/login')
+    return render_template('registration.html', title='Register')
+
     
